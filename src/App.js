673849001668efdac1b030f3a5256485bfae4e17
Moonlightsculptor88 <mahipreetham@gmail.com>
2021-05-28 18:06:29 +0530

    second

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import img from "./assets/background1.jpg"
+import './index.css';
+
+function App() {
+  return (
+    <div className="main-body" >
+      <p className="name">Mahi Pritam Reddy</p>
+      <h1>Design!</h1>
+        <p className="amazing">Amazing</p>
+      <p className="floating-number">01</p>
+        <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+    </div>
+  );
+}
+
+export default App;
